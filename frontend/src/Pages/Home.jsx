bd9b1748b1033583fee66f0293cf30522904aa22
Zuzana K <zk0433@gmail.com>
2024-01-22 12:54:47 +0000

    end of 02_04

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,8 +1,37 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 export default function Home() {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        axios
+            .get("https://jsonplaceholder.typicode.com/posts")
+            .then((response) => {
+                setPosts(response.data);
+            })
+            .catch((error) => {
+                console.error("Error fetching data:", error);
+            });
+    }, []);
+
     return (
         <div>
-            <h1>Welcome to my blog</h1>
-            <p>Check out my latest posts below</p>
+            <h1 className="my-5 text-xl font-semibold text-center">
+                Welcome to My Blog
+            </h1>
+
+            {posts &&
+                posts.map((post) => (
+                    <div
+                        key={post.id}
+                        className="p-5 my-5 border rounded-md shadow-sm text-left"
+                    >
+                        <h2 className="mb-5 font-bold">{post.title}</h2>
+
+                        <p>{post.body}</p>
+                    </div>
+                ))}
         </div>
     );
 }
